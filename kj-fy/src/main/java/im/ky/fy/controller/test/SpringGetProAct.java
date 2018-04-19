79cb18b7d3f5121be38e45acb8a86a73d1c145f5
yangjing <yangjing@gz-kf-yj.ZJHT.com>
2018-04-19 14:51:37 +0800

    @Value是Spring内部用来读取properties配置文件内容的注解

---
+++ kj-fy/src/main/java/im/ky/fy/controller/test/SpringGetProAct.java
@@ -0,0 +1,55 @@
+package im.ky.fy.controller.test;
+
+import im.ky.fy.service.serviceimpl.YjServiceImpl;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Value是Spring内部用来读取properties配置文件内容的注解
+ * @author yangjing
+ *
+ */
+@RequestMapping("sgp")
+@Controller
+public class SpringGetProAct {
+
+	@Autowired
+	YjServiceImpl yjService;
+
+	@RequestMapping("getpro.do")
+	@ResponseBody
+	public String getPro(HttpServletRequest request,HttpServletResponse response,ModelMap model){
+		
+		String X = yjService.url+"  "+yjService.userName+"  "+yjService.password;
+		
+		return X;
+		
+	}
+	
+	
+	/**********************************以下方式不能吧properties文件的spring配置信息注入到代码里面******************/
+	@Value("${url}")
+	public String url;
+	@Value("${userName}")
+	public String userName;
+	@Value("${password}")
+	public String password;
+	
+	@RequestMapping("getpro1.do")
+	@ResponseBody
+	public String getPro1(HttpServletRequest request,HttpServletResponse response,ModelMap model){
+		
+		String X = url+"  "+userName+"  "+password;
+		
+		return X;
+		
+	}
+}
---
+++ kj-fy/src/main/java/im/ky/fy/service/serviceimpl/YjServiceImpl.java
@@ -0,0 +1,20 @@
+package im.ky.fy.service.serviceimpl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Value是Spring内部用来读取properties配置文件内容的注解
+ * @author yangjing
+ *
+ */
+@Service
+public class YjServiceImpl {
+
+	@Value("${url}")
+	public String url;
+	@Value("${userName}")
+	public String userName;
+	@Value("${password}")
+	public String password;
+}
